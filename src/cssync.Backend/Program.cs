05d9b58fe0a4c27165bc2725714aab707970b0c2
Akeoott <akeoot@pm.me>
2025-11-22 16:44:48 +0100

    feat(backend/program): add initial `Program.cs` file
    - add initial `Program.cs` file
    - change backend from "class lib" to "console app"

---
+++ src/cssync.Backend/Program.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) Ame (Akeoott) <ame@akeoot.org>. Licensed under the GPLv3 License.
+// See the LICENSE file in the repository root for full license text.
+
+namespace cssync.Backend;
+
+internal class MainBackend
+{
+    internal static async Task Main(string[] args)
+    {
+
+    }
+
+}
