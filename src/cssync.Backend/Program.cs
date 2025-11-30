779d05814aa96b4baf138228d9d1768eb547d7a4
Akeoot <akeoot@pm.me>
2025-11-30 23:37:42 +0100

    refactor(backend/program) add startup logging and prepare HttpServer structure
    - Add logger messages during backend initialization
    - Print current process info on startup
    - Add HttpServer class placeholder for future implementation

--- src/cssync.Backend/Program.cs
+++ src/cssync.Backend/Program.cs
@@ -1,13 +1,23 @@
 ﻿// Copyright (c) Ame (Akeoott) <ame@akeoot.org>. Licensed under the GPLv3 License.
 // See the LICENSE file in the repository root for full license text.
 
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+using System.Net;
+
 namespace cssync.Backend;
 
 internal class MainBackend
 {
     internal static async Task Main(string[] args)
     {
-
+        Console.WriteLine(Process.GetCurrentProcess());
+        Globals.logger.LogInformation("Initiated Backend application.");
+        Globals.logger.LogInformation("Make sure rclone is configured. Use `rclone configure` to configure rclone.");
     }
+}
 
+public class HttpServer
+{
+    // TODO: Add HttpServer logic
 }
