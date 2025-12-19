11a9fdbe9119a0dfa470ef3b3c8325a72071f204
Akeoot <akeoot@pm.me>
2025-12-19 22:27:33 +0100

    refactor(logging): simplify logging to the point where it simply works the way it is intended
    - remove location specific logging as it is not possible to log them without issues this way

--- src/cssync.Backend/helper/Logging.cs
+++ src/cssync.Backend/helper/Logging.cs
@@ -1,7 +1,6 @@
 // Copyright (c) Ame (Akeoott) <ame@akeoot.org>. Licensed under the GPLv3 License.
 // See the LICENSE file in the repository root for full license text.
 
-using System.Runtime.CompilerServices;
 using Microsoft.Extensions.Logging;
 
 namespace cssync.Backend.helper;
@@ -16,69 +15,48 @@ public static class Log
                 options.TimestampFormat = "yyyy-MM-dd HH:mm:ss ";
                 options.IncludeScopes = false;
                 options.SingleLine = true;
-            });
+            }); builder.SetMinimumLevel(LogLevel.Information);
         })
     );
 
-    public static void Debug(string message,
-        [CallerMemberName] string member = "",
-        [CallerFilePath] string file = "",
-        [CallerLineNumber] int line = 0)
-        => Write(LogLevel.Debug, null, message, [], member, file, line);
+    public static void Debug(string message)
+        => Write(LogLevel.Debug, null, message, []);
 
     public static void Debug(string message, params object[] args)
         => Write(LogLevel.Debug, null, message, args);
 
-    public static void Info(string message,
-        [CallerMemberName] string member = "",
-        [CallerFilePath] string file = "",
-        [CallerLineNumber] int line = 0)
-        => Write(LogLevel.Information, null, message, [], member, file, line);
+    public static void Info(string message)
+        => Write(LogLevel.Information, null, message, []);
 
     public static void Info(string message, params object[] args)
         => Write(LogLevel.Information, null, message, args);
 
-    public static void Warn(string message,
-        [CallerMemberName] string member = "",
-        [CallerFilePath] string file = "",
-        [CallerLineNumber] int line = 0)
-        => Write(LogLevel.Warning, null, message, [], member, file, line);
+    public static void Warn(string message)
+        => Write(LogLevel.Warning, null, message, []);
 
     public static void Warn(string message, params object[] args)
         => Write(LogLevel.Warning, null, message, args);
 
-    public static void Error(string message,
-        [CallerMemberName] string member = "",
-        [CallerFilePath] string file = "",
-        [CallerLineNumber] int line = 0)
-        => Write(LogLevel.Error, null, message, [], member, file, line);
+    public static void Error(string message)
+        => Write(LogLevel.Error, null, message, []);
 
     public static void Error(string message, params object[] args)
         => Write(LogLevel.Error, null, message, args);
 
-    public static void Error(Exception ex, string message,
-        [CallerMemberName] string member = "",
-        [CallerFilePath] string file = "",
-        [CallerLineNumber] int line = 0)
-        => Write(LogLevel.Error, ex, message, [], member, file, line);
+    public static void Error(Exception ex, string message)
+        => Write(LogLevel.Error, ex, message, []);
 
     public static void Error(Exception ex, string message, params object[] args)
         => Write(LogLevel.Error, ex, message, args);
 
-    public static void Critical(string message,
-        [CallerMemberName] string member = "",
-        [CallerFilePath] string file = "",
-        [CallerLineNumber] int line = 0)
-        => Write(LogLevel.Critical, null, message, [], member, file, line);
+    public static void Critical(string message)
+        => Write(LogLevel.Critical, null, message, []);
 
     public static void Critical(string message, params object[] args)
         => Write(LogLevel.Critical, null, message, args);
 
-    public static void Critical(Exception ex, string message,
-        [CallerMemberName] string member = "",
-        [CallerFilePath] string file = "",
-        [CallerLineNumber] int line = 0)
-        => Write(LogLevel.Critical, ex, message, [], member, file, line);
+    public static void Critical(Exception ex, string message)
+        => Write(LogLevel.Critical, ex, message, []);
 
     public static void Critical(Exception ex, string message, params object[] args)
         => Write(LogLevel.Critical, ex, message, args);
@@ -87,20 +65,14 @@ public static class Log
         LogLevel level,
         Exception? exception,
         string message,
-        object[] args,
-        string member = "",
-        string file = "",
-        int line = 0)
+        object[] args)
     {
-        var fileName = Path.GetFileName(file);
-        var className = Path.GetFileNameWithoutExtension(file);
-        var logger = _loggerFactory.Value.CreateLogger(className);
-        var prefix = $"[{className}.{member} ({fileName}:{line})] ";
+        var logger = _loggerFactory.Value.CreateLogger(typeof(Log).FullName ?? "Log");
 
         logger.Log(
             level,
             exception,
-            prefix + message,
+            message,
             args
         );
     }
